9b3f78ab4fc454927c0388cc2dddcdf8ec885a1d
jhsong <wngml203@naver.com>
2023-03-02 17:31:54 +0900

    fix: conflict 해결 #16

--- src/main/java/com/linkyB/backend/match/controller/MatchController.java
+++ src/main/java/com/linkyB/backend/match/controller/MatchController.java
@@ -8,8 +8,6 @@ import com.linkyB.backend.match.service.MatchService;
 import com.linkyB.backend.user.presentation.dto.UserListDto;
 import com.linkyB.backend.user.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -27,35 +25,23 @@ public class MatchController {
     @PostMapping("/{userGetMatched}")
     public BaseResponse<MatchDto> matching(@PathVariable("userGetMatched") long userGetMatched) {
         long userId = SecurityUtil.getCurrentUserId();
-        System.out.println(userId);
+
         MatchDto response = matchService.matching(userId, userGetMatched);
         return new BaseResponse<>(response);
     }
 
     // 매칭 수락
     @PatchMapping("/accept/{userMatching}")
-<<<<<<< HEAD
     public BaseResponse<MatchDto> accept(@PathVariable("userMatching") long userMatching) {
         MatchDto response = matchService.accept(userMatching, SecurityUtil.getCurrentUserId());
         return new BaseResponse<>(response);
-=======
-    public ResponseEntity<MatchDto> accept(@PathVariable("userMatching") long userMatching) {
-        MatchDto response = matchService.accept(userMatching, SecurityUtil.getCurrentUserId());
-        return new ResponseEntity<>(response, HttpStatus.OK);
->>>>>>> master
     }
 
     // 매칭 거절
     @PostMapping("/refuse/{userMatching}")
-<<<<<<< HEAD
     public BaseResponse<BlockDto> refuse(@PathVariable("userMatching") long userMatching) {
         BlockDto response = matchService.refuse(SecurityUtil.getCurrentUserId(),userMatching);
         return new BaseResponse<>(response);
-=======
-    public ResponseEntity<BlockDto> refuse(@PathVariable("userMatching") long userMatching) {
-        BlockDto response = matchService.refuse(SecurityUtil.getCurrentUserId(),userMatching);
-        return ResponseEntity.ok().body(response);
->>>>>>> master
     }
 
     // 모든 매칭 수락
@@ -67,15 +53,9 @@ public class MatchController {
 
     // 내가 매칭 시도한 내역 삭제
     @PostMapping("/block/{userGetMatched}")
-<<<<<<< HEAD
     public BaseResponse<BlockDto> matchBlock(@PathVariable("userGetMatched") long userGetMatched) {
         BlockDto response = matchService.blockMatch(SecurityUtil.getCurrentUserId(),userGetMatched);
         return new BaseResponse<>(response);
-=======
-    public ResponseEntity<BlockDto> matchBlock(@PathVariable("userGetMatched") long userGetMatched) {
-        BlockDto response = matchService.blockMatch(SecurityUtil.getCurrentUserId(),userGetMatched);
-        return ResponseEntity.ok().body(response);
->>>>>>> master
     }
 
     // 나에게 매칭 시도한 유저 전체 조회
@@ -89,11 +69,7 @@ public class MatchController {
     @GetMapping("/Matching/all")
     public BaseResponse<List<UserListDto>> MatchingList() {
         List<UserListDto> response = matchService.MatchingList(SecurityUtil.getCurrentUserId());
-<<<<<<< HEAD
         return new BaseResponse<>(response);
-=======
-        return ResponseEntity.ok(response);
->>>>>>> master
     }
 
     // 매칭 내역 리스트 조회 (매칭 홈)
