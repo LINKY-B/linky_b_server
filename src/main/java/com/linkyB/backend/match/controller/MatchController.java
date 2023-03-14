41753eaac555ae563ef65eb28a4c43338b93cbda
jhsong <wngml203@naver.com>
2023-03-15 02:10:04 +0900

    fix: MatchController api 토큰 인증 변경#24"

--- src/main/java/com/linkyB/backend/match/controller/MatchController.java
+++ src/main/java/com/linkyB/backend/match/controller/MatchController.java
@@ -5,6 +5,7 @@ import com.linkyB.backend.config.BaseResponse;
 import com.linkyB.backend.match.dto.MatchDto;
 import com.linkyB.backend.match.dto.MatchListDto;
 import com.linkyB.backend.match.service.MatchService;
+import com.linkyB.backend.user.jwt.JwtTokenProvider;
 import com.linkyB.backend.user.presentation.dto.UserListDto;
 import com.linkyB.backend.user.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
@@ -20,55 +21,62 @@ import java.util.Map;
 public class MatchController {
 
     private final MatchService matchService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     // 매칭 시도
     @PostMapping("/{userGetMatched}")
     public BaseResponse<MatchDto> matching(@PathVariable("userGetMatched") long userGetMatched) {
-        long userId = SecurityUtil.getCurrentUserId();
+        long TokenUser = jwtTokenProvider.getUser();
 
-        MatchDto response = matchService.matching(userId, userGetMatched);
+        MatchDto response = matchService.matching(TokenUser, userGetMatched);
         return new BaseResponse<>(response);
     }
 
     // 매칭 수락
     @PatchMapping("/accept/{userMatching}")
     public BaseResponse<MatchDto> accept(@PathVariable("userMatching") long userMatching) {
-        MatchDto response = matchService.accept(SecurityUtil.getCurrentUserId(), userMatching);
+        long TokenUser = jwtTokenProvider.getUser();
+        MatchDto response = matchService.accept(TokenUser, userMatching);
         return new BaseResponse<>(response);
     }
 
     // 매칭 거절
     @PostMapping("/refuse/{userMatching}")
     public BaseResponse<BlockDto> refuse(@PathVariable("userMatching") long userMatching) {
-        BlockDto response = matchService.refuse(SecurityUtil.getCurrentUserId(),userMatching);
+        long TokenUser = jwtTokenProvider.getUser();
+        BlockDto response = matchService.refuse(TokenUser,userMatching);
         return new BaseResponse<>(response);
     }
 
     // 모든 매칭 수락
     @PatchMapping("/all")
     public BaseResponse<MatchListDto> matckAllOk() {
-        MatchListDto response = matchService.all(SecurityUtil.getCurrentUserId());
+        long TokenUser = jwtTokenProvider.getUser();
+        MatchListDto response = matchService.all(TokenUser);
         return new BaseResponse<>(response);
     }
 
     // 내가 매칭 시도한 내역 삭제
     @PostMapping("/block/{userGetMatched}")
     public BaseResponse<BlockDto> matchBlock(@PathVariable("userGetMatched") long userGetMatched) {
-        BlockDto response = matchService.blockMatch(SecurityUtil.getCurrentUserId(),userGetMatched);
+        long TokenUser = jwtTokenProvider.getUser();
+        BlockDto response = matchService.blockMatch(TokenUser,userGetMatched);
         return new BaseResponse<>(response);
     }
 
     // 나에게 매칭 시도한 유저 전체 조회
     @GetMapping("/getMatched/all")
     public BaseResponse<List<UserListDto>> GetMatchedList() {
-        List<UserListDto> response = matchService.GetMatchedList(SecurityUtil.getCurrentUserId());
+        long TokenUser = jwtTokenProvider.getUser();
+        List<UserListDto> response = matchService.GetMatchedList(TokenUser);
         return new BaseResponse<>(response);
     }
 
     // 내가 매칭 시도한 유저 전체 조회
     @GetMapping("/Matching/all")
     public BaseResponse<List<UserListDto>> MatchingList() {
-        List<UserListDto> response = matchService.MatchingList(SecurityUtil.getCurrentUserId());
+        long TokenUser = jwtTokenProvider.getUser();
+        List<UserListDto> response = matchService.MatchingList(TokenUser);
         return new BaseResponse<>(response);
     }
 
@@ -76,9 +84,9 @@ public class MatchController {
     @GetMapping("/main")
     public BaseResponse<Map<String, List<UserListDto>>> getDoubleList() {
         Map<String, List<UserListDto>> response = new HashMap<>();
-        long userId = SecurityUtil.getCurrentUserId();
-        response.put("나에게 연결을 시도한 회원", matchService.homeGetMatchedList(userId));
-        response.put("내가 연결을 시도한 회원", matchService.homeMatchingList(userId));
+        long TokenUser = jwtTokenProvider.getUser();
+        response.put("나에게 연결을 시도한 회원", matchService.homeGetMatchedList(TokenUser));
+        response.put("내가 연결을 시도한 회원", matchService.homeMatchingList(TokenUser));
 
         return new BaseResponse<>(response);
     }
